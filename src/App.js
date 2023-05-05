23e62e6a2b160b62cf05fb4bf6c1113ee302672c
sathish <2000sathishkumar.d@gmail.com>
2023-05-05 15:04:26 +0530

    add pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 import React from "react";
+// Add pages 
+import Home from "./pages/Home";
+import Detail from "./pages/Detail";
+import AddEditBlog from "./pages/AddEditBlog";
+import About from "./pages/About";
+import NotFound from "./pages/NotFound"
+// Add Routes the react dom
+import { Routes, Route } from "react-router-dom";
 
 const App = () => {
-  return <div className="text-2xl my-5 font-bold underline">App</div>;
+  return (
+    <div className="text-2xl my-5 font-bold underline">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/detail/:id" element={<Detail />} />
+        <Route path="/create" element={<AddEditBlog />} />
+        <Route path="/update/:id" element={<AddEditBlog />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </div>
+  )
 };
 
 export default App;
